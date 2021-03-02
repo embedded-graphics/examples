5c2201ede972355f14c0a46194568ee033a281e9
James Waples <james@wapl.es>
2021-03-02 14:36:25 +0000

    Add 0.6 examples

---
+++ eg-0.6/examples/primitives-fill-macros.rs
@@ -0,0 +1,112 @@
+//! # Example: Using macros to define and style primitives
+//!
+//! Demonstrate usage of macros to create primitives and associated styles. The final output is the
+//! same as the `primitives-fill` example.
+
+use embedded_graphics::{
+    egcircle, egrectangle, egtriangle, pixelcolor::BinaryColor, prelude::*, primitive_style,
+};
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+
+static CIRCLE_SIZE: i32 = 32;
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(384, 128));
+
+    egcircle!(
+        center = (CIRCLE_SIZE, CIRCLE_SIZE),
+        radius = CIRCLE_SIZE as u32,
+        style = primitive_style!(stroke_color = BinaryColor::On, stroke_width = 1,)
+    )
+    .draw(&mut display)?;
+
+    egcircle!(
+        center = (CIRCLE_SIZE, CIRCLE_SIZE),
+        radius = CIRCLE_SIZE as u32,
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::On,
+        )
+    )
+    .translate(Point::new(16, 16))
+    .draw(&mut display)?;
+
+    egcircle!(
+        center = (CIRCLE_SIZE, CIRCLE_SIZE),
+        radius = CIRCLE_SIZE as u32,
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::Off
+        )
+    )
+    .translate(Point::new(CIRCLE_SIZE, CIRCLE_SIZE))
+    .draw(&mut display)?;
+
+    egrectangle!(
+        top_left = (0, 0),
+        bottom_right = (64, 64),
+        style = primitive_style!(stroke_color = BinaryColor::On, stroke_width = 1,)
+    )
+    .translate(Point::new(96, 0))
+    .draw(&mut display)?;
+
+    egrectangle!(
+        top_left = (0, 0),
+        bottom_right = (64, 64),
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::On
+        )
+    )
+    .translate(Point::new(96 + 16, 16))
+    .draw(&mut display)?;
+
+    egrectangle!(
+        top_left = (0, 0),
+        bottom_right = (64, 64),
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::Off
+        )
+    )
+    .translate(Point::new(96 + 32, 32))
+    .draw(&mut display)?;
+
+    egtriangle!(
+        points = [(32, 0), (0, 64), (64, 64)],
+        style = primitive_style!(stroke_color = BinaryColor::On, stroke_width = 1,)
+    )
+    .translate(Point::new(96 * 2, 0))
+    .draw(&mut display)?;
+
+    egtriangle!(
+        points = [(32, 0), (0, 64), (64, 64)],
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::On,
+        )
+    )
+    .translate(Point::new(96 * 2 + 16, 16))
+    .draw(&mut display)?;
+
+    egtriangle!(
+        points = [(32, 0), (0, 64), (64, 64)],
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::Off,
+        )
+    )
+    .translate(Point::new(96 * 2 + 32, 32))
+    .draw(&mut display)?;
+
+    let output_settings = OutputSettingsBuilder::new().scale(2).build();
+    Window::new("Filled primitives using macros", &output_settings).show_static(&display);
+
+    Ok(())
+}
---
+++ eg-0.6/examples/primitives-stroke.rs
@@ -0,0 +1,84 @@
+//! # Example: Primitive stroke styles
+//!
+//! This example demonstrates the different stroke styles available for primitives.
+
+use embedded_graphics::{
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, Line, Rectangle, Triangle},
+    style::PrimitiveStyle,
+};
+use embedded_graphics_simulator::{OutputSettings, SimulatorDisplay, Window};
+
+const PADDING: i32 = 16;
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(320, 256));
+
+    let thin_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
+    let medium_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 3);
+    let thick_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 10);
+
+    let triangle = Triangle::new(Point::new(0, 64), Point::new(64, 0), Point::new(64, 64));
+    let rectangle =
+        Rectangle::new(Point::new(0, 0), Point::new(64, 64)).translate(Point::new(64 + PADDING, 0));
+    let line = Line::new(Point::new(0, 0), Point::new(64, 64))
+        .translate(Point::new((64 + PADDING) * 2, 0));
+    let circle = Circle::new(Point::new(32, 32), 32).translate(Point::new((64 + PADDING) * 3, 0));
+
+    circle
+        .into_styled(thin_stroke)
+        .into_iter()
+        .chain(rectangle.into_styled(thin_stroke).into_iter())
+        .chain(line.into_styled(thin_stroke).into_iter())
+        .chain(triangle.into_styled(thin_stroke).into_iter())
+        .draw(&mut display)?;
+
+    let offset = Point::new(0, 64 + PADDING);
+    circle
+        .translate(offset)
+        .into_styled(medium_stroke)
+        .into_iter()
+        .chain(
+            rectangle
+                .translate(offset)
+                .into_styled(medium_stroke)
+                .into_iter(),
+        )
+        .chain(
+            line.translate(offset)
+                .into_styled(medium_stroke)
+                .into_iter(),
+        )
+        .chain(
+            triangle
+                .translate(offset)
+                .into_styled(medium_stroke)
+                .into_iter(),
+        )
+        .draw(&mut display)?;
+
+    let offset = Point::new(0, (64 + PADDING) * 2);
+    circle
+        .translate(offset)
+        .into_styled(thick_stroke)
+        .into_iter()
+        .chain(
+            rectangle
+                .translate(offset)
+                .into_styled(thick_stroke)
+                .into_iter(),
+        )
+        .chain(line.translate(offset).into_styled(thick_stroke).into_iter())
+        .chain(
+            triangle
+                .translate(offset)
+                .into_styled(thick_stroke)
+                .into_iter(),
+        )
+        .draw(&mut display)?;
+
+    Window::new("Strokes", &OutputSettings::default()).show_static(&display);
+
+    Ok(())
+}
