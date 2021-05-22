b022cb9e0b9e4d6f8e15c871becc3c4b5e3b71e7
James Waples <james@wapl.es>
2021-05-22 12:25:43 +0100

    Add nice colours to stroked primitives demo

--- eg-next/examples/primitives-stroke.rs
+++ eg-next/examples/primitives-stroke.rs
@@ -3,7 +3,7 @@
 //! This example demonstrates the different stroke styles available for primitives.
 
 use embedded_graphics::{
-    pixelcolor::BinaryColor,
+    pixelcolor::Rgb888,
     prelude::*,
     primitives::{
         Circle, CornerRadii, Ellipse, Line, PrimitiveStyle, Rectangle, RoundedRectangle, Triangle,
@@ -14,27 +14,27 @@ use embedded_graphics_simulator::{OutputSettings, SimulatorDisplay, Window};
 const PADDING: i32 = 16;
 
 /// Draws all embedded-graphics primitives.
-fn draw_primitives<D>(target: &mut D, style: PrimitiveStyle<BinaryColor>) -> Result<(), D::Error>
+fn draw_primitives<D>(target: &mut D, w: u32) -> Result<(), D::Error>
 where
-    D: DrawTarget<Color = BinaryColor>,
+    D: DrawTarget<Color = Rgb888>,
 {
     Triangle::new(Point::new(0, 64), Point::new(64, 0), Point::new(64, 64))
-        .into_styled(style)
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_ORANGE_RED, w))
         .draw(target)?;
 
     Rectangle::new(Point::new(0, 0), Size::new(64, 64))
         .translate(Point::new(64 + PADDING, 0))
-        .into_styled(style)
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_GOLD, w))
         .draw(target)?;
 
     Line::new(Point::new(0, 0), Point::new(64, 64))
         .translate(Point::new((64 + PADDING) * 2, 0))
-        .into_styled(style)
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_SEA_GREEN, w))
         .draw(target)?;
 
     Circle::new(Point::new(0, 0), 64)
         .translate(Point::new((64 + PADDING) * 3, 0))
-        .into_styled(style)
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_TEAL, w))
         .draw(target)?;
 
     RoundedRectangle::new(
@@ -42,21 +42,17 @@ where
         CornerRadii::new(Size::new(16, 16)),
     )
     .translate(Point::new((64 + PADDING) * 4, 0))
-    .into_styled(style)
+    .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_STEEL_BLUE, w))
     .draw(target)?;
 
     Ellipse::new(Point::new(0, 0), Size::new(96, 64))
         .translate(Point::new((64 + PADDING) * 5, 0))
-        .into_styled(style)
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_FUCHSIA, w))
         .draw(target)
 }
 
 fn main() -> Result<(), core::convert::Infallible> {
-    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(512, 256));
-
-    let thin_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    let medium_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 3);
-    let thick_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 10);
+    let mut display = SimulatorDisplay::<Rgb888>::new(Size::new(512, 256));
 
     // Draw the primitives using a thin stroke.
     //
@@ -64,15 +60,15 @@ fn main() -> Result<(), core::convert::Infallible> {
     // using `display.translated(position)`. This translates all drawing operations in `draw_shapes`
     // by 10 pixels in the x and y direction.
     let mut position = Point::new(10, 10);
-    draw_primitives(&mut display.translated(position), thin_stroke)?;
+    draw_primitives(&mut display.translated(position), 1)?;
 
     // Draw the primitives using a medium stroke.
     position.y += 64 + PADDING;
-    draw_primitives(&mut display.translated(position), medium_stroke)?;
+    draw_primitives(&mut display.translated(position), 3)?;
 
     // Draw the primitives using a thick stroke.
     position.y += 64 + PADDING;
-    draw_primitives(&mut display.translated(position), thick_stroke)?;
+    draw_primitives(&mut display.translated(position), 10)?;
 
     Window::new("Strokes", &OutputSettings::default()).show_static(&display);
 
